33cde880abcd0383aaf58e3d6a9b4389126d1141
Ali Abbaspour <aliabbaspourzx9090@gmail.com>
2026-08-14 11:20:30 +0330

    Add comments for files

--- src/run.py
+++ src/run.py
@@ -9,10 +9,11 @@ from PIL import Image
 # --- Main Page ---
 st.title("Pytopia Dashboard")
 
-
+# --- banner ---
 banner = Image.open('./data/images.jfif')
 st.image(banner, use_container_width=True)
 
+# --- login ---
 login_option = st.sidebar.radio('Login/SignUp', ('Login', 'SignUp'))
 
 if login_option == 'Login':
@@ -38,6 +39,7 @@ else:
         if submitted:
             pass
 
+# --- statistics of members of pytopia's telegram group ---
 with st.expander('Statistics'): 
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
@@ -48,6 +50,7 @@ with st.expander('Statistics'):
         data = json.loads(string_data)
         st.json(data)
 
+# --- statistic ---
 with st.expander('Statistics2'):
     fig, ax = plt.subplots(1, 1, figsize=(10, 5))
     sns.histplot(np.random.randn(100), ax=ax)
@@ -55,6 +58,7 @@ with st.expander('Statistics2'):
 
 st.metric(label="Pytopia members", value="16000", delta="+100")
 
+# --- User Info ---
 with st.expander('User profile'):
     st.write('welcome to your profile')
     col1, col2, = st.columns(2)
